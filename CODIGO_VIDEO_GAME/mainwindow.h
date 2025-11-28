51cd73ae3bf16c1639d68228a1b5b85ab488a60e
SantiHenaoZ <santiago.henaoz@udea.edu.co>
2025-11-28 12:26:40 -0500

    se crea el menu con los niveles y las ecenas para los niveles

--- CODIGO_VIDEO_GAME/mainwindow.h
+++ CODIGO_VIDEO_GAME/mainwindow.h
@@ -3,21 +3,39 @@
 
 #include <QMainWindow>
 
-QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
-QT_END_NAMESPACE
+class QStackedWidget;
+class QWidget;
+class QPushButton;
+class QLabel;
 
 class MainWindow : public QMainWindow
 {
     Q_OBJECT
-
 public:
-    MainWindow(QWidget *parent = nullptr);
+    explicit MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    void resizeEvent(QResizeEvent *event) override;
+
+private slots:
+    void onLevel1();
+    void onLevel2();
+    void onLevel3();
+    void onBackToMenu();
+
 private:
-    Ui::MainWindow *ui;
+    void setupMenu();
+    QWidget* createLevelWidget(int level);
+
+    QStackedWidget *stack;
+    QWidget *menuWidget;
+    QPushButton *backButton;
+
+    // botones del menu (opcional para futuros cambios)
+    QPushButton *btnLevel1;
+    QPushButton *btnLevel2;
+    QPushButton *btnLevel3;
 };
+
 #endif // MAINWINDOW_H
